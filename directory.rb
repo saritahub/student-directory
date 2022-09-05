67159ca85341c880de0c573b426e1f4707466dca
Sarita <saritaradia65@gmail.com>
2022-09-05 17:49:50 +0100

    New method to refactor the student array

--- directory.rb
+++ directory.rb
@@ -1,17 +1,33 @@
 #Student names in an array
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
+# students = [
+#     {name: "Dr. Hannibal Lecter", cohort: :november},
+#     {name: "Darth Vader", cohort: :november},
+#     {name: "Nurse Ratched", cohort: :november},
+#     {name: "Michael Corleone", cohort: :november},
+#     {name: "Alex DeLarge", cohort: :november},
+#     {name: "The Wicked Witch of the West", cohort: :november},
+#     {name: "Terminator", cohort: :november},
+#     {name: "Freddy Krueger", cohort: :november},
+#     {name: "The Joker", cohort: :november},
+#     {name: "Joffrey Baratheon", cohort: :november},
+#     {name: "Norman Bates", cohort: :november}
+# ]
+
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice" #So 'name' will be empty, and while loop breaks 
+    students = []
+    name = gets.chomp
+    
+    while !name.empty? do 
+        #New hash pushed to students array
+        #key = name: = value of name, value = cohort: :november (hardcoded)
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end
+    students #return array of hashes of the students 
+end 
 
 #Print list of students
 def print_header 
@@ -31,6 +47,8 @@ def print_footer(names)  #names placeholder
     puts "Overall, we have #{names.count} great students"
 end 
 
+students = input_students #The outcome of the method input_students 
 print_header
-print(students)
-print_footer(students)
+print(students) #Again, outcome of method input_students
+print_footer(students) #Outcome of method input_students
+
