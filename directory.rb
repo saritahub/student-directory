cf4f1b3b4744f9e9812a68a5d9d49abd50249e15
Sarita <saritaradia65@gmail.com>
2022-09-05 16:45:46 +0100

    Refactored name values to an array, and iterated over the array to print student names

--- directory.rb
+++ directory.rb
@@ -1,18 +1,25 @@
-student_count = 11
-#Firstly, print list of students
+#Student names in an array
+students = [
+    "Dr. Hannibal Lecter",
+    "Darth Vader",
+    "Nurse Ratched",
+    "Michael Corleone",
+    "Alex DeLarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffrey Baratheon",
+    "Norman Bates"
+]
+
+#Print list of students
 puts "The students of Villans Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+#Iterate over the students array to print each name
+students.each do |student|
+    puts student
+end 
 
-#Finally, we print the total number of students
-puts "Overall, we have #{student_count} great students"
+#Finally, we print the total number of students using the array.count method
+puts "Overall, we have #{students.count} great students"
