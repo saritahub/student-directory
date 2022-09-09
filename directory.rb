723c2983c01f99c9e49289ebe5f5f50b2e0b4f99
Sarita <saritaradia65@gmail.com>
2022-09-09 17:32:01 +0100

    Added students.csv file to save students list

--- directory.rb
+++ directory.rb
@@ -7,6 +7,7 @@
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit"
 end 
 
@@ -17,12 +18,24 @@ def show_students
     print_footer(@students) #Outcome of method input_students
 end 
 
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end 
+    file.close 
+end 
+
 def process(selection)
     case selection
     when "1"
       input_students
     when "2"
       show_students
+    when "3"
+        save_students
     when "9"
       exit
     else  
@@ -164,6 +177,8 @@ def print_footer(names)  #names placeholder
         puts "No student information yet"
     end 
 end 
+
+
 
 interactive_menu
 students = input_students #The outcome of the method input_students 
